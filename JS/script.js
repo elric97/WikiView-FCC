d793e527aa730edd4aff05e074960f5c2ccea16c
elric97 <sharma.rachit882@gmail.com>
2017-06-17 15:42:29 +0530

    updated

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,13 @@
 $(document).ready(function()
 {
-	
+  	$("#rand").click(function()
+  	{
+		window.open("https://en.wikipedia.org/wiki/Special:Random","_blank");
+  	});	
 	$("#srch").click(function()
 	{
-    var query=$("#query").val();
+   		$("ul").empty();
+    	var query=$("#query").val();
 		if (query==='')
 		{
 			$("#noval").show();
@@ -12,23 +16,30 @@ $(document).ready(function()
 		$.ajax(
 		{
 			type: 'GET',
-      url: 'https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=' + query,
-      dataType: 'jsonp',
-			success: function(result) {
-        console.log(result);
-        var obj = result.query.pages;
-        for (var prop in obj) {
-          $("ul").append('<a target="_blank" href="https://en.wikipedia.org/wiki?curid=' + obj[prop].pageid + '">' + '<li><h3 class="title ">' + obj[prop].title + '</h3><p class="text">' + obj[prop].extract + '</p></li></a>');
-        }
-      }
+      		url: 'https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=' + query,
+      		dataType: 'jsonp',
+			success: function(result) 
+			{
+        		var obj = result.query.pages; //get the number of pages
+        		for (var prop in obj) 
+				{//iterate throught page
+        			$("ul").append('<a target="_blank" href="https://en.wikipedia.org/wiki?curid=' + obj[prop].pageid + '">' + '<li><h3 class="title ">' + obj[prop].title + '</h3><p class="text">' + obj[prop].extract + '</p></li></a>');
+        			//creating links of the li elements here and makin the whole element a link
+				}
+      		}
 		});
 	});
 	$('.searchbar').keypress(function(e) 
 	{
-    	if (e.which == 13) 
+    	if (e.which == 13) //for searching when pressing the enter key
     	{
-      		$('#search').click();
+      		$('#srch').click();
       		return false;
     	}
   	});
+  
+   $("ul li").mouseenter(function() //not working correctly will look into it 
+   {
+        $(this).fadeTo("slow",1);  
+   }) 
 });
